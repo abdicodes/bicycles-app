84a4ab03172b0b1b873a889d6d31dac872d34aef
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-05-04 04:08:01 +0300

    server side pagination init

--- controllers/stations.js
+++ controllers/stations.js
@@ -2,12 +2,29 @@ const router = require('express').Router()
 const { Station } = require('../models')
 const { Op } = require('sequelize')
 const sequelize = require('sequelize')
+const { getPagination, getPagingData } = require('../utils/paging')
 
 router.get('/:id', async (req, res) => {
   try {
     const trip = await Station.findByPk(req.params.id, {})
 
     res.send(trip)
+  } catch (e) {
+    console.log(e)
+    res.status(400).end()
+  }
+})
+
+router.get('/', async (req, res) => {
+  try {
+    const { limit, offset } = getPagination(5, 10)
+    const data = await Station.findAndCountAll({
+      limit,
+      offset,
+    })
+    const stations = getPagingData(data, 5, limit)
+
+    res.send(stations)
   } catch (e) {
     console.log(e)
     res.status(400).end()
--- controllers/trips.js
+++ controllers/trips.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const { Trip, Station } = require('../models')
 const { Op } = require('sequelize')
 const sequelize = require('sequelize')
+const { getPagination, getPagingData } = require('../utils/paging')
 
 router.get('/:id', async (req, res) => {
   try {
@@ -11,6 +12,30 @@ router.get('/:id', async (req, res) => {
         { model: Station, as: 'returnStation' },
       ],
     })
+
+    res.send(trip)
+  } catch (e) {
+    console.log(e)
+    res.status(400).end()
+  }
+})
+
+router.get('/', async (req, res) => {
+  try {
+    const { limit, offset } = getPagination(10, 5)
+    const data = await Trip.findAndCountAll({
+      include: [
+        { model: Station, as: 'departureStation' },
+        { model: Station, as: 'returnStation' },
+      ],
+      limit,
+      offset,
+      order: [
+        // Will escape title and validate DESC against a list of valid direction parameters
+        ['id', 'ASC'],
+      ],
+    })
+    const trip = getPagingData(data, 10, limit)
 
     res.send(trip)
   } catch (e) {
