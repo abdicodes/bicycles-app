cfef3f4c668c284494b1358c4506fcbc597a51ca
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-05-08 20:58:19 +0300

    UI components for adding new trips and stations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import StationList from './components/Stations'
 import TripList from './components/Trips'
 import SingleStation from './components/SingleStation'
 import { Routes, Route } from 'react-router-dom'
+import NewStation from './components/NewStation'
+import NewTrip from './components/NewTrip'
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
           <Route path="/" element={<StationList />} />
           <Route path="/trips" element={<TripList />} />
           <Route path="/stations/:id" element={<SingleStation />} />
+          <Route path="/add-station" element={<NewStation />} />
+          <Route path="/add-trip" element={<NewTrip />} />
         </Routes>
       </>
     </div>
