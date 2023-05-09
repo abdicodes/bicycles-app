66bbf385f6d84e1094483f4025ffcffcfc1c5730
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-05-09 15:11:01 +0300

    delete files to solve git issue

--- models/index.js
+++
@@ -1,9 +0,0 @@
-const Station = require('./stations')
-const Trip = require('./trips')
-
-Trip.belongsTo(Station, { foreignKey: 'departureId', as: 'departureStation' })
-Trip.belongsTo(Station, { foreignKey: 'returnId', as: 'returnStation' })
-module.exports = {
-  Station,
-  Trip,
-}
